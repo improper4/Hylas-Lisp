570bf6ccdb8256c05c662929ee7907198c609f2c
Fernando Borretti <eudoxiahp@gmail.com>
2013-07-03 17:20:20 -0300

    Add skeleton testing file

---
+++ t/hylas.lisp
@@ -0,0 +1,17 @@
+#|
+  This file is a part of hylas project.
+  Copyright (c) 2013 Fernando Borretti (eudoxiahp@gmail.com)
+|#
+
+(in-package :cl-user)
+(defpackage hylas-test
+  (:use :cl
+        :hylas
+        :cl-test-more))
+(in-package :hylas-test)
+
+(plan nil)
+
+;; blah blah blah.
+
+(finalize)
