71ec821caa29e16766e6ae1b2e7bf6659edde301
Fernando Borretti <eudoxiahp@gmail.com>
2013-07-27 18:32:22 -0300

    Minor changes

--- src/fn.lisp
+++ src/fn.lisp
@@ -1,13 +1,16 @@
 (in-package :hylas)
 
 (defun integer-constructorp (fn)
-    (and (equalp #\i (elt (symbol-name fn) 0))
-        (handler-case
-            (parse-integer (subseq (symbol-name fn) 1))
-        (error () nil))))
+  (and (equalp #\i (elt (symbol-name fn) 0))
+       (handler-case
+         (parse-integer (subseq (symbol-name fn) 1))
+         (error () nil))))
 
 (defun corep (fn code)
-    (gethash (symbol-name fn) (core code)))
+  (gethash (symbol-name fn) (core code)))
 
-(defun specialp (fn code)
-    (gethash (symbol-name fn) (special code)))
+(defun operatorp (fn code)
+  (gethash (symbol-name fn) (operators code)))
+
+(defmethod callfn (fn form (code <code>))
+  (error "Eudoxia hasn't got this far yet 😿"))
