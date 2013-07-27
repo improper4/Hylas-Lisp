33823ad398a4dc00e977af9b880f02c23aad6bb8
Fernando Borretti <eudoxiahp@gmail.com>
2013-07-27 18:32:56 -0300

    More utils

--- src/utils.lisp
+++ src/utils.lisp
@@ -1,3 +1,24 @@
+(in-package :hylas)
+(annot:enable-annot-syntax)
+
 (defmacro aif (test true-branch &optional false-branch)
   `(let ((it ,test))
      (if it ,true-branch ,false-branch)))
+
+(defun pairs (list)
+  (if (cdr list)
+      (append (list (cons (car list) (cadr list)))
+              (pairs (cddr list)))
+      nil))
+
+(defun hash (&rest pairs)
+  (let ((out (make-hash-table :test #'equal)))
+    (loop for pair in (pairs pairs) do
+          (setf (gethash (car pair) out) (cdr pair)))
+    out))
+
+@export
+@doc "Set the `docstring` of *package* to hold the documentation text."
+(cl-annot:defannotation document (text)
+  (:arity 1 :inline t)
+  `(setf (documentation *package* t) ,text))
