5a035935160c8d682a1a405e3f15d25590688112
Fernando Borretti <eudoxiahp@gmail.com>
2013-08-03 09:36:06 -0300

    Change predicate style

--- src/fn.lisp
+++ src/fn.lisp
@@ -1,15 +1,15 @@
 (in-package :hylas)
 
-(defun integer-constructorp (fn)
+(defun integer-constructor? (fn)
   (and (equalp #\i (elt (symbol-name fn) 0))
        (handler-case
          (parse-integer (subseq (symbol-name fn) 1))
          (error () nil))))
 
-(defun corep (fn code)
+(defun core? (fn code)
   (gethash (symbol-name fn) (core code)))
 
-(defun operatorp (fn code)
+(defun operator? (fn code)
   (gethash (symbol-name fn) (operators code)))
 
 (defmethod callfn (fn form (code <code>))
