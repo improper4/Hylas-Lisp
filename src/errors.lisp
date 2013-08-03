136b932b675921f642a92ec5198dc4399a23bdba
Fernando Borretti <eudoxiahp@gmail.com>
2013-08-03 09:33:33 -0300

    Add error for type errors in core functions and operators

--- src/errors.lisp
+++ src/errors.lisp
@@ -2,3 +2,7 @@
 
 (defmacro raise (code msg &rest args)
   `(error (format nil ,(concatenate 'string msg "~%Code state:~%~a") ,@args ,code)))
+
+(defun bad-input-type (code name expected-type pos given-type)
+  (error code "(~A) expected ~A as its ~A argument, but got ~A"
+    expected-type pos given-type))
