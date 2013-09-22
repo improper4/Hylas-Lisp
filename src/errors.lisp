010e3e7169e0adbee2041c7babea014a1138182c
Fernando Borretti <eudoxiahp@gmail.com>
2013-09-22 09:09:49 -0300

    Pretty-print hylas errors

--- src/errors.lisp
+++ src/errors.lisp
@@ -1,7 +1,8 @@
 (in-package :hylas)
 
 (define-condition hylas-error (error)
-  ((msg :initarg :msg :reader msg)))
+  ((msg :initarg :msg :reader msg))
+  (:report (lambda (condition stream) (format stream "~A" (msg condition)))))
 
 (defmacro raise (code msg &rest args)
   `(error 'hylas-error
